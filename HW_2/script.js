0f82f78648b8f5c12699a9d8ecb320cb21e40bf9
Siirusho <74008883+Siirusho@users.noreply.github.com>
2021-07-14 15:04:11 +0400

    Update script.js

--- HW_2/script.js
+++ HW_2/script.js
@@ -1,42 +1,25 @@
-const myEvent = require('events');
-const logger = new myEvent();
-const CLI = new myEvent();
-const users = [];
-const msgs = [];
+const events = require('events');
+const EventEmitter = events.EventEmitter;
+const myEvent = new EventEmitter();
 
-logger.on('message', (msg)=>{
-    console.log('new message', msg);
-    msgs.push(msg);
-})
+const user = [];
+const msg = [];
+const argv = require('minimist')(process.argv.slice(2));
+let userFromTerminal = argv.addUser;
+let messageFromTerminal = argv.message;
 
-logger.on('login', (name)=>{
-    console.log('new user', name);
-    users.push(name);
-})
+function addingMessageInMsg(ms) {
+    msg.push(ms)
+}
+myEvent.on('message', addingMessageInMsg);
+myEvent.emit('message', messageFromTerminal)
 
-logger.on('getUsers', () => {
-    console.log('Logged user', ...users);
-} )
-
-logger.on('getMessage', () => {
-    console.log('Messages', ...msgs);
-})
-
-CLI.on('AddUser', (name) => {
-    console.log('Hire User Name');
-    users.push(name);
-})
-CLI.on('Message', (msg)=>{
-    console.log('Hire User Messsage');
-    msgs.push(msg);
-})
-
-logger.emit('message', 'Hello world!');
-logger.emit('login', 'Bob');
-logger.emit('getUsers');
-logger.emit('getMessage');
-CLI.emit('AddUser', 'Max');
-CLI.emit('Message', 'Hi!');
-
-
-
+function addingUserInUser(us) {
+    user.push(us)
+}
+myEvent.on('user', addingUserInUser);
+myEvent.emit('user', userFromTerminal)
+console.log(user);
+console.log(msg);
+console.log(argv);
+// node response.js  --addUser "Marsel"  --message "Welcome"
