4b64d5eec0c265a693c294ceada2c7dfecea87dd
Sirusho <sirushomartikyan77@gmail.com>
2021-07-17 17:54:41 +0400

    Homework3

--- HW_3/script.js
+++ HW_3/script.js
@@ -4,10 +4,23 @@ const path = require('path');
 try{
     let dataPath=path.join(__dirname, 'Data', 'addresses.csv');
     const data = fs.readFileSync(dataPath);
-    var array = data.toString().split("\n");
+    let array = data.toString().split("\n");
     let json = JSON.stringify(array);
     console.log(json);
 }catch(e){
     console.error(e);
 }
 
+function allFilesFolders(folder){
+  if(!fs.lstatSync(folder).isDirectory() ) return;
+  fs.readdirSync(folder).forEach(file => {
+  console.log(file);
+  let nestedPath=path.join(folder, file);
+  allFilesFolders(nestedPath);
+});
+}
+function print(folder){
+  console.log(folder);
+  allFilesFolders(folder);
+}
+console.log(print('node_modules'));
