2904232482ecc2e3957ecffd4aedb15a824fd789
Sirusho <sirushomartikyan77@gmail.com>
2021-07-18 11:07:16 +0400

    Homework 3

--- HW_3/script.js
+++ HW_3/script.js
@@ -1,16 +1,38 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
-try{
-    let dataPath=path.join(__dirname, 'Data', 'addresses.csv');
-    const data = fs.readFileSync(dataPath);
-    let array = data.toString().split("\n");
-    let json = JSON.stringify(array);
-    console.log(json);
-}catch(e){
-    console.error(e);
+//1.Read file content. File path should be provided via readline.
+function readFileViaReadline(){
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question('Which  file do you want to read ', (fileName) => {
+    fs.readFile(fileName, 'utf8', (err, data) => {
+        if (err) console.log('err', err);
+        console.log(data);
+        rl.close();
+    })
+  });
 }
+//readFileViaReadline()
 
+//2.Convert attached .csv file to .json file
+function convert(dataPath){
+    try{
+        const data = fs.readFileSync(dataPath);
+        let array = data.toString()
+        let json = JSON.stringify(array);
+        return json;
+    }catch(e){
+        console.error(e);
+    }
+}
+dataPath=path.join(__dirname, 'Data', 'addresses.csv')
+//console.log(convert(dataPath))
+
+//3.Print attached directory structure
 function allFilesFolders(folder){
   if(!fs.lstatSync(folder).isDirectory() ) return;
   fs.readdirSync(folder).forEach(file => {
@@ -23,4 +45,4 @@ function print(folder){
   console.log(folder);
   allFilesFolders(folder);
 }
-console.log(print('node_modules'));
+// console.log(print('node_modules'));
