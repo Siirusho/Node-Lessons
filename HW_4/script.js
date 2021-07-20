8c4702c8d998b0740d0dd931787254238c04122e
Sirusho <sirushomartikyan77@gmail.com>
2021-07-20 20:42:39 +0400

    Homework 4

---
+++ HW_4/script.js
@@ -0,0 +1,21 @@
+const cluster = require('cluster');
+const process = require('process');
+
+const factorial = (num) => {
+    if(num==1 || num==0) return num
+    return num*factorial(num-1)
+}
+if (cluster.isMaster) {
+    const worker = cluster.fork();
+    worker.on('listening', () => {
+        worker.send('calculating');
+        worker.disconnect();
+    }); 
+} else if (cluster.isWorker) {
+    const net = require('net');
+    const server = net.createServer().listen(8000);
+
+    process.on('message', (msg) => {
+        if (msg === 'calculating') console.log(factorial(50))
+    });
+}
