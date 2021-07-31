6c03c3fe53180ae25ac6525fd172497e610d8a9a
Sirusho <sirushomartikyan77@gmail.com>
2021-07-31 16:32:42 +0400

    Homework 7 Express

--- HW_7/script.js
+++ HW_7/script.js
@@ -10,21 +10,21 @@ app.get('/countries', (req, res)=>{
   fs.readFile('./data.json', 'utf-8', (err, countries)=>{
     if(err) console.log(err);
     res.send(countries);
-  })
+  });
 })
 
 app.post('/countries', (req, res)=>{
   fs.writeFile('./data.json', JSON.stringify(req.body, undefined, 2), 'utf8', err =>{
     if(err) console.log(err);
     res.send('received');
-  })
+  });
 })
 
 app.patch('/countries', (req, res)=>{
   fs.writeFile('./data.json', JSON.stringify(req.body, undefined, 2), 'utf8', err =>{
     if(err) console.log(err);
       res.send('changed');
-  })
+  });
 })
 
 app.listen(port, () => {
